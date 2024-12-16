785c07512e15a2a122e8c62747fe7c4fc9701503
antonmyronyuk <antonmyronyuk@gmail.com>
2024-12-16 13:12:07 +0200

    Use heap for day16

--- day16/solution.py
+++ day16/solution.py
@@ -1,5 +1,5 @@
 import itertools
-from collections import deque
+from heapq import heappush, heappop
 
 with open("input.txt") as input_file:
     field = [list(line) for line in input_file.read().strip().split("\n")]
@@ -8,13 +8,13 @@ n, m = len(field), len(field[0])
 start = next((i, j) for i in range(n) for j in range(m) if field[i][j] == "S")
 end = next((i, j) for i in range(n) for j in range(m) if field[i][j] == "E")
 dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-queue = deque([(start, (0, 1), 0, [])])  # (position, direction, score, path)
+queue = [(0, start, (0, 1), [])]  # (score, position, direction, path)
 cost_map = {}  # (position, direction): score
 paths_score_pairs = []
 
 
 while queue:
-    pos, dir, score, path  = queue.popleft()
+    score, pos, dir, path  = heappop(queue)
     if (pos, dir) in cost_map and cost_map[pos, dir] < score:
         continue
 
@@ -28,7 +28,8 @@ while queue:
     for di, dj in dirs:
         ni, nj = pos[0] + di, pos[1] + dj
         if field[ni][nj] in (".", "E"):
-            queue.append(((ni, nj), (di, dj), score + (1 if dir == (di, dj) else 1001), path))
+            next_score = score + (1 if dir == (di, dj) else 1001)
+            heappush(queue, (next_score, (ni, nj), (di, dj), path))
 
 
 min_cost = min(cost_map[end, dir] for dir in dirs if (end, dir) in cost_map)
